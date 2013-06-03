a93b9ae22e420200f3ff492497f25ef4f5a7dfb2
Kostya <Kostya@Vodin>
2013-06-03 16:57:52 +0300

    upd

--- DrawLinesFromMySQLDB/src/org/ossystem/it/ui/MainPaint.java
+++ DrawLinesFromMySQLDB/src/org/ossystem/it/ui/MainPaint.java
@@ -8,35 +8,38 @@ import javax.swing.*;
 import org.ossystem.it.db.PointDAOImpl;
 import org.ossystem.it.model.Point;
 
-public class PaintLines {
+public class MainPaint {
 
-	DrawPanel  pp;
+	DrawPanel  drawPanel;
 	JButton jbtnAdd, jbtnEdit, jbtnDelete;
 	JTextField jtf = new JTextField(10);
 	Random rand = new Random();
 	JTable jTabPoints;
 	PointTable pTable = new PointTable(PointDAOImpl.getInstance().getAllPoints());
 	
-	PaintLines()  {
+	MainPaint()  {
 		
-		JFrame  jfrm =  new JFrame("Paint Lines");
-		jtf.setVisible(false);
+		JFrame  jfrm =  new JFrame("Paint Lines");		
 		jfrm.setLayout(null);
 		jfrm.setSize(800,  800);
 		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		jtf.setVisible(false);
+		
 		jbtnAdd = new JButton("Add point");
 		jbtnDelete = new JButton("Delete point");
-		pp = new DrawPanel();
-		pp.setBounds(15, 15, 300, 300);
-		jfrm.add(pp);
 		
+		drawPanel = new DrawPanel();
+		drawPanel.setBounds(15, 15, 300, 300);
+		jfrm.add(drawPanel);
+				
 		jTabPoints = new JTable(pTable);
-		jTabPoints.setBounds(350, 15, 300, 300);
+		jTabPoints.setBounds(350, 15, 300, 800);
 		
 		jbtnAdd.setBounds(15, 350, 120, 30);
 		jbtnDelete.setBounds(170, 350, 120, 30);
-		jtf.setBounds(15, 450, 100, 30);
 		
+		jtf.setBounds(170, 390, 120, 30);		
 		
 		jbtnAdd.addActionListener(new ActionListener() {
 			@Override
@@ -45,15 +48,13 @@ public class PaintLines {
 				p.setName("New Point");
 				p.setXX(rand.nextInt(300));
 				p.setYY(rand.nextInt(300));
-				PointDAOImpl.getInstance().addPoint(p);
-				//jTabPoints.updateUI();
+				PointDAOImpl.getInstance().addPoint(p);				
 				jTabPoints.setModel(new PointTable(PointDAOImpl.getInstance().getAllPoints()));
 				jTabPoints.repaint();
-				pp.updateUI();
+				drawPanel.updateUI();
 			}
 		});	
-		
-		
+				
 		jbtnDelete.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
@@ -63,11 +64,12 @@ public class PaintLines {
 					public void actionPerformed(ActionEvent ae) {					
 						int id = Integer.parseInt(jtf.getText());
 						PointDAOImpl.getInstance().deletePoint(id);						
-						pp.updateUI();
+						drawPanel.updateUI();
+						jTabPoints.setModel(new PointTable(PointDAOImpl.getInstance().getAllPoints()));
+						jTabPoints.repaint();
 						jtf.setVisible(false);
 					}
-				}) ;
-				jTabPoints.updateUI();
+				}) ;				
 			}
 		});
 						
@@ -75,15 +77,13 @@ public class PaintLines {
 		jfrm.add(jbtnDelete);
 		jfrm.add(jtf);
 		jfrm.add(jTabPoints);		
-		jfrm.setVisible(true);
-		
+		jfrm.setVisible(true);		
 	}
-	
-	
+		
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable()  {
 			public void run()  {
-				new PaintLines();
+				new MainPaint();
 			}
 		});
 	}
